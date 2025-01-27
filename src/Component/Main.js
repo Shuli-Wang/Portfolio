3936672b83f3aa3a558962d79d62d6b13fcc21f0
Shuli-Wang <julieswang00@gmail.com>
2025-01-26 20:21:03 -0500

    revise the intro part of main page

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -7,15 +7,16 @@ function MainPage() {
     <div className="Main">
 
       <div className="container-basic">
-        <h1 style={{ fontWeight: 'normal' }}>Bugs are crafty, lens caps are slippery — good thing I’m a professional at both hide-and-seek and keeping track.</h1>
+        <h1>Bugs are crafty, lens caps are slippery — good thing I’m a professional at both hide-and-seek and keeping track.</h1>
         <div className="container-main">
-          <h1 style={{ marginTop: '0', background: 'linear-gradient(to bottom, transparent 50%, #d2dbd0 50%' }}>Quick Facts</h1>
-          <p>1. I’m actively seeking a full-time Junior Software Developer role anywhere in Ontario, Canada.</p>
-          <p>2. I experienced in full-stack development, database management and version control — skills I’ve applied to projects that solve real problems (and occasinally something just for fun).</p>
-          <p>3. I may not tick every box for the role, but I’m a quick learner, a careful listener, detail-oriented, and focused on executing not excuses.</p>
-          <p>4. I’m flexible—open to travel, on-call duties, and any extra effort needed to contribute meaningfully.</p>
-          <p>5. I build websites, operate cameras, but still failed to plug the USB in on the first try.</p>
-
+          <h1 style={{fontWeight:'bold', marginTop: '0', background: 'linear-gradient(to bottom, transparent 50%, #d2dbd0 50%' }}>Quick Facts</h1>
+          <p>• My name is Shuli, a software developer with a passion for both coding and photography. You can find more about me in the About section in the header</p>
+          <p>• I’m actively seeking a full-time Junior Software Developer role anywhere in Ontario, Canada, including remote opportunities.</p>
+          <p>• I have hands-on experience in full-stack development, database management, and version control  — skills I’ve applied to projects that solve real problems (and occasionally something just for fun).</p>
+          <p>• While I may not tick every box for a role, I’m a quick learner, a careful listener, detail-oriented, and focused on execution, not excuses.</p>
+          <p>• I’m flexible — open to travel, on-call duties, and any extra effort needed to contribute meaningfully.</p>
+          <p>• I build websites, operate cameras, but still failed to plug the USB in on the first try.</p>
+          <h1 style={{marginLeft:'0',marginRight:'0'}}>Feel free to explore my projects, check out my photography, or reach out — I’d love to connect!</h1>
 
         </div>
       </div>
