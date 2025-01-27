051d0cad2753bf9e5837c058e1fba01042147241
Shuli-Wang <julieswang00@gmail.com>
2025-01-26 22:56:52 -0500

    Minor change on Main page

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -10,7 +10,7 @@ function MainPage() {
         <h1>Bugs are crafty, lens caps are slippery — good thing I’m a professional at both hide-and-seek and keeping track.</h1>
         <div className="container-main">
           <h1 style={{fontWeight:'bold', marginTop: '0', background: 'linear-gradient(to bottom, transparent 50%, #d2dbd0 50%' }}>Quick Facts</h1>
-          <p>• My name is Shuli, a software developer with a passion for both coding and photography. You can find more about me in the <a href="/About">About</a> Section.</p>
+          <p>• My name is Shuli, a software developer with a passion for both coding and photography. You can find more about me in the <a href="/About">About</a> section.</p>
           <p>• I’m actively seeking a full-time Junior Software Developer role anywhere in Ontario, Canada, including remote opportunities.</p>
           <p>• I have hands-on experience in full-stack development, database management, and version control  — skills I’ve applied to projects that solve real problems (and occasionally something just for fun).</p>
           <p>• While I may not tick every box for a role, I’m a quick learner, a careful listener, detail-oriented, and focused on execution, not excuses.</p>
