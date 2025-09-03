52b9c3ded8bf3b0b588a25c61228c5c50460a9e9
Shuli-Wang <julieswang00@gmail.com>
2025-09-03 01:21:42 -0400

    update menu bar alt text

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -27,7 +27,7 @@ const Header = () => {
           <button className="logo"></button>
         </Link>
 
-        <button className="menu-toggle" alt="menu" onClick={toggleMenu}>
+        <button className="menu-toggle" aria-label="menu" onClick={toggleMenu}>
           {isMenuOpen ? "✖" : "☰"}
         </button>
         <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
